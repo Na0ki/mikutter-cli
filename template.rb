9456191ac883ef333f4de828c06fbb159e531138
ahiru <wellingtonnmaeda0524@gmail.com>
2018-11-18 17:53:33 +0900

    put all self method together to class << self

--- template.rb
+++ template.rb
@@ -2,43 +2,46 @@
 
 require 'erb'
 require 'fileutils'
-require 'yaml'
 require 'pathname'
+require 'yaml'
 
 module MikutterCLI
   # Handle template
   class Template
-    LOAD_DIR = Pathname(__dir__) / 'template'
+    class << self
+      TEMPLATE_DIR = Pathname(__dir__) / 'template'
 
-    def self.confroot
-      Pathname(File.expand_path(Environment::CONFROOT || ENV['MIKUTTER_CONFROOT'] || '~/.mikutter'))
-    rescue
-      Pathname(File.expand_path(ENV['MIKUTTER_CONFROOT'] || '~/.mikutter'))
-    end
+      # conf root of mikutter
+      # @param root
+      # @return Pathname
+      def conf_root(root = nil)
+        Pathname(File.expand_path(root || Environment::CONFROOT || ENV['MIKUTTER_CONFROOT'] || '~/.mikutter'))
+      rescue
+        Pathname(File.expand_path(ENV['MIKUTTER_CONFROOT'] || '~/.mikutter'))
+      end
 
-    def self.load(filename, model)
-      erb = File.read LOAD_DIR / filename
-      ERB.new(erb).result(OpenStruct.new(model.to_h).instance_eval { binding })
-    end
+      # load ERB template
+      # @param filename template filename
+      # @param model
+      # @return ERB
+      def load(filename, model)
+        erb = File.read TEMPLATE_DIR / filename
+        ERB.new(erb).result(OpenStruct.new(model.to_h).instance_eval { binding })
+      end
 
-    def self.write(filename, model)
-      plugin = load filename, model
-      puts confroot
-      plugin_dir = confroot / 'plugin' / model.slug
-      puts plugin_dir
-      FileUtils.mkdir_p(plugin_dir)
-      File.write plugin_dir / "#{model.slug}.rb", plugin
-    end
-
-    def self.generate_plugin(filename, model)
-      plugin = load filename, model
-      plugin_dir = confroot / 'plugin' / model.slug
-      FileUtils.mkdir_p(plugin_dir)
-      File.open(plugin_dir / '.mikutter.yml', 'w') { |f| YAML.dump model.to_h_k_s, f }
-      File.write plugin_dir / "#{model.slug}.rb", plugin
-      puts "Plugin has been generated at #{plugin_dir}"
-    rescue StandardError => e
-      puts e.message
+      # generate plugin
+      # @param filename
+      # @param model
+      def generate_plugin(filename, model)
+        plugin = load filename, model
+        plugin_dir = conf_root / 'plugin' / model.slug
+        FileUtils.mkdir_p(plugin_dir)
+        File.open(plugin_dir / '.mikutter.yml', 'w') { |f| YAML.dump model.to_h_k_s, f }
+        File.open(plugin_dir / "#{model.slug}.rb", 'w') { |f| f.puts plugin }
+        puts "Plugin has been generated at #{plugin_dir}"
+      rescue StandardError => e
+        p e
+      end
     end
   end
 end
